017c30d0c775bc13872fdc50d223f7d786b85b0b
Brad Leege <bleege@gmail.com>
2017-01-19 22:51:27 -0600

    Initial Commit

---
+++ Ligue1Stadiums/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ligue1Stadiums
+//
+//  Created by Brad Leege on 1/19/17.
+//  Copyright © 2017 Brad Leege. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
