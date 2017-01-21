a7338b909bea1c29537d249aa4624c73f1f63565
Brad Leege <bleege@gmail.com>
2017-01-21 10:40:23 -0600

    Adding custom table cell for club maps

---
+++ Ligue1Stadiums/ClubMapTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ClubMapTableViewCell.swift
+//  Ligue1Stadiums
+//
+//  Created by Brad Leege on 1/21/17.
+//  Copyright © 2017 Brad Leege. All rights reserved.
+//
+
+import UIKit
+
+class ClubMapTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
